9035211e8e35ac24e54c709405df217deef365d3
kimyena1123 <kyq104@gmail.com>
2023-01-13 19:50:54 +0900

    feat: configuration 어노테이션 적용

--- src/main/java/com/yena/test/SpringTestApplication.java
+++ src/main/java/com/yena/test/SpringTestApplication.java
@@ -1,17 +1,8 @@
 package com.yena.test;
 
-import javax.sql.DataSource;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-@MapperScan(basePackages="com.yena.test.*")
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) // db연동 하기 전
 @SpringBootApplication
 public class SpringTestApplication {
@@ -19,15 +10,5 @@ public class SpringTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApplication.class, args);
 	}
-
-	@Bean
-	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-		sessionFactory.setDataSource(dataSource);
-		
-		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
-		sessionFactory.setMapperLocations(res);
-		
-		return sessionFactory.getObject();
-	}
+	
 }
